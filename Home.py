ca209cfd0131c249cb779d77cc6a7f68d58faa24
SANDEEP KUMAR <107092701+sandeeprairai@users.noreply.github.com>
2024-04-30 12:39:23 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+                   page_title="Gurgaon Real Estate  Analytics Page",
+                   page_icon="😁"
+                   )
+st.write("# Welcome to streamlit Page")
+
+st.sidebar.success("Select a demo above")
+
